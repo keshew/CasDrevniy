aac11e5a62ca7b462f55b11b137c03b2bf9972b2
keshew <artyom.korotckov2017@yandex.ru>
2025-11-20 12:55:58 +0500

    Initial Commit

---
+++ CasDrevniy/CasDrevniyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasDrevniyApp.swift
+//  CasDrevniy
+//
+//  Created by Артём Коротков on 20.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CasDrevniyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CasDrevniy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CasDrevniy
+//
+//  Created by Артём Коротков on 20.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
